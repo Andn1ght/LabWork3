433b527cdc5b4e54add35685a75798e0d0c7359f
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-30 19:39:57 +0600

    creating a constructor for the class and initialize the instance variable

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,3 +1,7 @@
 public class MyLinkedListQueue<E> implements QueueInterface<E> {
     private MyLinkedList<E> queue;
+
+    public MyLinkedListQueue() {
+        queue = new MyLinkedList<>();
+    }
 }
