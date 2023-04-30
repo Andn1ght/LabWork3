c607b034225f191138909766a73301511536dfd6
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-30 19:24:56 +0600

    creating a constructor for the class and initialize the instance variable

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,3 +1,6 @@
 public class MyLinkedListStack<E> implements StackInterface<E> {
     private MyLinkedList<E> myLinkedList;
+    public MyLinkedListStack(){
+        myLinkedList = new MyLinkedList<>();
+    }
 }
