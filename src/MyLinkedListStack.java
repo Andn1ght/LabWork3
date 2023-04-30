7466082f7048cb09c1f1c5bb1560aa1c7c32f485
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-30 19:26:14 +0600

    implementing the push method that adds an element to the top of the stack using the addFirst method for MyLinkedList

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,6 +1,12 @@
 public class MyLinkedListStack<E> implements StackInterface<E> {
     private MyLinkedList<E> myLinkedList;
+
     public MyLinkedListStack(){
         myLinkedList = new MyLinkedList<>();
+    }
+
+    @Override
+    public void push(E element) {
+        myLinkedList.addFirst(element);
     }
 }
