382408dc3f27aa2e28383e0a951ebe580772016e
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-30 19:22:16 +0600

    commenting all rows from StackInterface

--- src/StackInterface.java
+++ src/StackInterface.java
@@ -1,7 +1,17 @@
 public interface StackInterface<E> {
+
+    // Returns the number of elements in the stack.
     int getSize();
+
+    // Returns the number of elements in the stack.
     boolean isEmpty();
+
+    // Pushes the specified element onto the top of the stack.
     void push(E element);
+
+    // Removes and returns the element at the top of the stack.
     E pop();
+
+    // Returns the element at the top of the stack without removing it.
     E peek();
 }
