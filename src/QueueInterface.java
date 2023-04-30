6890a6fd7ab1a480e7e9a3c41e6fcb1231158248
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-30 19:37:22 +0600

    commenting all rows from QueueInterface

--- src/QueueInterface.java
+++ src/QueueInterface.java
@@ -1,7 +1,19 @@
 public interface QueueInterface<E> {
+
+    // Adds an element to the end of the queue.
     void enqueue(E element);
+
+    // Removes and returns the element at the front of the queue.
+    // Throws a NoSuchElementException if the queue is empty.
     E dequeue();
+
+    // Returns the element at the front of the queue without removing it.
+    // Throws a NoSuchElementException if the queue is empty.
     E peek();
+
+    // Returns true if the queue is empty, false otherwise.
     boolean isEmpty();
+
+    // Returns the number of elements in the queue.
     int getSize();
 }
